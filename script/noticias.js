95886744dd3ff454ee3963edda0c7209b3a19eb7
arthurritzel <ritzelarthur@gmail.com>
2023-09-27 01:43:04 -0300

    noticias_dinamico

---
+++ script/noticias.js
@@ -0,0 +1,45 @@
+const URL = "https://rpstdm9a.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27noticias%27%5D%7B%0A++titulo%2C%0A++subtitulo%2C%0A++conteudo%2C%0A++%22imagem%22%3A+imagem.asset-%3Eurl%2C%0A++data%0A%0A%7D"
+
+
+async function noticias(){
+    var dados = await fetch(URL, {
+        method: "GET"
+    })
+
+    var dadosJS = await dados.json()
+    dadosJS.result.forEach(element => {
+        var div_txt = document.createElement("div");
+        div_txt.className = "txt";
+        var div_noticia = document.createElement("div");
+        div_noticia.className = "noticia"
+        
+        var titulo = document.createElement("p");
+        titulo.className = "titulo";
+        var subtitulo = document.createElement("p");
+        subtitulo.className = "subtitulo";
+        var descricao = document.createElement("p");
+        descricao.className = "descricao";
+        var data = document.createElement("p");
+        data.className = "data";
+        var img = document.createElement("img");
+        img.className = "img-noticia"
+        
+        titulo.innerText = element.titulo;
+        subtitulo.innerText = element.subtitulo;
+        descricao.innerText = element.conteudo;
+        data.innerText = element.data;
+        img.setAttribute("src", element.imagem);
+
+
+        div_txt.appendChild(titulo)
+        div_txt.appendChild(subtitulo)
+        div_txt.appendChild(descricao)
+        div_txt.appendChild(data)
+
+        div_noticia.appendChild(div_txt);
+        div_noticia.appendChild(img);
+
+        document.body.appendChild(div_noticia)
+    });
+}
+noticias();
