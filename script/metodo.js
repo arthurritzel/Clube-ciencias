632d9a1d561d3f4cb64d33940fe3341d2f79ea21
arthurritzel <ritzelarthur@gmail.com>
2023-10-03 19:45:16 -0300

    Create metodo.js

---
+++ script/metodo.js
@@ -0,0 +1,13 @@
+const url = "https://rpstdm9a.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%27metodo%27%5D%7B%0A++descricao%0A%7D"
+async function metodo(){
+    var dados = await fetch(url,{
+        method: "GET"
+    }
+    )
+var dados = await dados.json();
+
+
+
+
+
+}
