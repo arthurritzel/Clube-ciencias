1048f4affd7cd8045d4ae92084dd090236cd5505
arthurritzel <ritzelarthur@gmail.com>
2023-10-03 19:45:33 -0300

    Update metodo.js

--- script/metodo.js
+++ script/metodo.js
@@ -1,6 +1,6 @@
-const url = "https://rpstdm9a.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%27metodo%27%5D%7B%0A++descricao%0A%7D"
+const URL = "https://rpstdm9a.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%27metodo%27%5D%7B%0A++descricao%0A%7D"
 async function metodo(){
-    var dados = await fetch(url,{
+    var dados = await fetch(URL,{
         method: "GET"
     }
     )
